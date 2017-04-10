7b992a4e444649337453afa791c29a8b0544faae
Richard Dvorsky <generator@mydevelops.com>
2017-04-10 11:22:43 +0200

    NEW activator deactivator

---
+++ includes/class-semantify_it-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       www.semantify.it
+ * @since      1.0.0
+ *
+ * @package    Semantify_it
+ * @subpackage Semantify_it/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Semantify_it
+ * @subpackage Semantify_it/includes
+ * @author     Richard Dvorsky <richard.dvorsky@sti2.at>
+ */
+class Semantify_it_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
