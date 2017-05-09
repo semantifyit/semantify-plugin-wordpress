d89d442a35804433937e826641daa110803255f5
Richard Dvorsky <generator@mydevelops.com>
2017-05-09 11:27:21 +0200

    NEW: notice for api key in more condtitions

--- admin/partials/semantify_it-admin-notices.php
+++ admin/partials/semantify_it-admin-notices.php
@@ -6,8 +6,27 @@
  * Time: 14:40
  */
 
+$admin_url = admin_url( 'admin.php?page=' . $this->plugin_name );
+
+
     if(!extension_loaded('curl')) {
         echo "<br/>";
         $this->h->displayMessage("error","<b>".__('No cURL installed!', $this->plugin_name)."</b>"."<br>".__('This plugin requires cURL library for getting annotations. Without this library, the semantify.it plugin will not work!', $this->plugin_name) );
         die();
     }
+
+
+if( ((isset($annotations)) && (count($annotations)<=3)) || $admin ){
+
+    if(!$Semantify->isApiKeyValid()){
+        echo"</br>";
+        if($admin){
+            $this->h->displayMessage("error","<b>".__('Wrong API key', $this->plugin_name)."</b>"."<br>".__('You added an API which is not a correct one.', $this->plugin_name) );
+            echo"</br>";
+        }else{
+            $this->h->displayMessage("error","<b>".__('Wrong API key', $this->plugin_name)."</b>"."<br>".__('You added an API which is not a correct one. Please add a new API key in ', $this->plugin_name)."<a href='".$admin_url."'>Settings</a>" );
+            die();
+        }
+
+    }
+}
